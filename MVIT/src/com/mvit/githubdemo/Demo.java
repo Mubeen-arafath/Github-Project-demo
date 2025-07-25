cfb65e59eb7c49c0da34831bdad81799eb9ba4dd
haswinhaswin014@gmail.com <aswin@LAPTOP-ADOQ49B2>
2025-07-25 14:32:52 +0530

    push

--- MVIT/src/com/mvit/githubdemo/Demo.java
+++ MVIT/src/com/mvit/githubdemo/Demo.java
@@ -3,8 +3,9 @@ package com.mvit.githubdemo;
 public class Demo {
 
 	public static void main(String[] args) {
-
-System.out.println("MVIT_C66");
+		
+      System.out.println("MVIT_C66");
+      System.out.println("CoreJava");
      
 	}
 
